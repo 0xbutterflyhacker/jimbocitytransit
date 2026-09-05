3f9e39d73d1ec6aeea13ae9b14c69601782ef676
Chantal Jenkins <60161241+cjenkin1@users.noreply.github.com>
2026-09-05 15:33:31 -0400

    added some work on omny card joker

--- jokers/fares.lua
+++ jokers/fares.lua
@@ -1,6 +1,28 @@
 SMODS.Joker { --MetroCard
-    key = "metrocard",
+    key = 'metrocard',
+    loc_txt = {
+        name = 'MetroCard'
+    },
     pos = { x = 0, y = 0 },
     rarity = 1,
     cost = 2
+}
+
+SMODS.Joker {
+    key = 'omny',
+    loc_txt = {
+        name = 'OMNY Card',
+        text = { 'If deck has at most 35 cards, {C:chips}+30 {}Chips and {C:mult}+5 {}Mult',
+            'per Route Card held' }
+    },
+    pos = { x = 0, y = 0 },
+    rarity = 2,
+    cost = 4,
+    config = { extra = { chips = 30, mult = 5 } },
+
+    calculate = function(self, card, context)
+        if context.other_joker then
+
+        end
+    end
 }
